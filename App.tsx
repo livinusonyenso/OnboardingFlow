3da143998c6ce9e18e0a2a7c3036cea86161e52b
livinusonynso <livinusonyenso@gmail.com>
2026-03-16 11:34:16 +0100

    fix: add react-native-reanimated babel plugin to resolve infinite loading spinner

--- App.tsx
+++ App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { LogBox } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { OnboardingNavigator } from './src/navigation';
+
+LogBox.ignoreLogs(['ExpoKeepAwake']);
 
 export default function App() {
   return (
